552bee112dd557073890beda094d0f86a03bec5d
mac <mac@macdemacbook-pro.local>
2015-12-07 14:45:24 +0800

    Initial Commit

---
+++ JCImageSliderView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JCImageSliderView
+//
+//  Created by mac on 15/12/7.
+//  Copyright © 2015年 Jcdroid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JCImageSliderView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JCImageSliderView
+//
+//  Created by mac on 15/12/7.
+//  Copyright © 2015年 Jcdroid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
